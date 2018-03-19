8fa483b9d1f429849ad79dd2fa3e63d05947c2ca
Anne Kotecha <anne.kotecha@gmail.com>
2018-03-19 15:53:09 +0000

    Add new files.

--- projects/parallax-02/js/index.js
+++ projects/parallax-02/js/index.js
@@ -8,13 +8,11 @@ window.requestAnimationFrame = window.requestAnimationFrame
  || window.msRequestAnimationFrame
  || function(f){setTimeout(f, 1000/60)}
  
-var bubble1 = document.getElementById('back');
-var bubble2 = document.getElementById('fore');
-
-
+var bubble1 = document.getElementById('bubbles1')
+var bubble2 = document.getElementById('bubbles2')
  
 function parallaxbubbles(){
-	console.log('buubble1 ', bubble1);
+	console.log('parallax bubbles');
  var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically 
  bubble1.style.top = -scrolltop * .2 + 'px' // move bubble1 at 20% of scroll rate
  bubble2.style.top = -scrolltop * .5 + 'px' // move bubble2 at 50% of scroll rate
